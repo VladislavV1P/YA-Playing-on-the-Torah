736689046f4e17d0a5c0b8adba8b8a4f6e6ae02e
Vladislav Patrakov <gostreid1@gmail.com>
2023-01-20 16:13:47 +0300

    develop solution 5

--- Plaing on the Torah.playground/Contents.swift
+++ Plaing on the Torah.playground/Contents.swift
@@ -42,7 +42,6 @@ func generateObstaclePoints() -> [Array<Int>: Int] {
 var obstaclePoints  = generateObstaclePoints()
 obstaclePoints[finishPoint] = 2
 
-var completedStep  = [(point: startPoint, pathTraveled: " ", stepCost: 0)]
 var shortPath = "-1"
 
 var noWay = false
@@ -50,115 +49,150 @@ var finishStep = false
 
 var checkStep = (finish: false, stepWay: true)
 
+func costSoFar(currentPoint: [Int], sizeTorah: [Int], finish: [Int]) -> Int {
+    let midleSizeRow = sizeTorah[0] / 2
+    let midleSizeCol = sizeTorah[1] / 2
+    let deltaRow = finish[0] - midleSizeRow
+    let deltaCol = finish[1] - midleSizeCol
+    let xRow = abs(currentPoint[0] - finish[0])
+    let yCol = abs(currentPoint[1] - finish[1])
+    let sum = xRow + yCol
+    let sumX = abs(xRow - midleSizeRow + deltaRow - finish[0])
+    let sumY = abs(yCol - midleSizeCol + deltaCol - finish[1])
+    let sumXY = sumX + sumY
+    
+    if xRow <= midleSizeRow && yCol <= midleSizeCol {
+        return sum
+    } else if  xRow > midleSizeRow && yCol > midleSizeCol{
+        return sumXY
+    } else if xRow > midleSizeRow {
+      return sumX + abs(currentPoint[1] - finish[1])
+    } else {
+       return sumY + abs(currentPoint[0] - finish[0])
+    }
+}
+
+let сostStarPoint = costSoFar(currentPoint: startPoint, sizeTorah: sizeTorah, finish: finishPoint)
+
+var completedStep  = [(point: startPoint, pathTraveled: " ", stepCost: сostStarPoint)]
+
+var wayPoints = completedStep
+var passedPoints = Set<Array<Int>>()
+var totalFinish = false
+var stepPath = true
+
 func stepVerification(step: (point: [Int], pathTraveled: String, stepCost: Int)) {
     
-    var totalFinish = false
-    var stepPath = true
-    
-    if !totalFinish {
-        switch obstaclePoints[step.point] {
-        case 0:
-            stepPath = true
-        case 1:
-            stepPath = false
-        case 2:
-            shortPath = step.pathTraveled
-            totalFinish = true
-        case .none:
-            print("none")
-        case .some(_):
-            print("some")
-        }
+    switch obstaclePoints[step.point] {
+    case 0:
+        stepPath = true
+    case 1:
+        stepPath = false
+    case 2:
+        shortPath = step.pathTraveled
+        totalFinish = true
+    case .none:
+        print("none")
+    case .some(_):
+        print("some")
     }
+    
     checkStep = (finish: totalFinish, stepWay: stepPath)
 }
 
-var noDoublePoint = Set<[Int]>()
-
-func findingPath(way: [(point: [Int], pathTraveled: String, stepCost: Int)]) {
+func findingPath(way: (point: [Int], pathTraveled: String, stepCost: Int)) {
     
-    var total = 0
     var stepWay = [(point: [Int], pathTraveled: String, stepCost: Int)]()
     let wayString = ["S","N","E","W"]
     let invertSade = ["N","S","W","E"]
     let wayInt = [1,-1,1,-1]
     checkStep = (finish: false, stepWay: true)
+    var step = way
+    checkStep.stepWay = true
     
-    for step in way {
-        checkStep.stepWay = true
+    for index in wayString.indices {
+        var newStep = step
+        let lastChar = newStep.pathTraveled.last ?? "Q"
+        if String(lastChar) == invertSade[index] {
+            continue
+        }
+        newStep.pathTraveled += wayString[index]
         
-        
-        for index in wayString.indices {
-            var newStep = step
-            let lastChar = newStep.pathTraveled.last ?? "Q"
-            if String(lastChar) == invertSade[index] {
-                continue
+        if index < 2 {
+            switch newStep.point[0] + wayInt[index] {
+            case sizeTorah[0]:
+                newStep.point[0] = 0
+            case -1:
+                newStep.point[0] = sizeTorah[0] - 1
+            case 0..<sizeTorah[0]:
+                newStep.point[0] += wayInt[index]
+            default:
+                print("error")
             }
-            newStep.pathTraveled += wayString[index]
-            
-            if index < 2 {
-                switch newStep.point[0] + wayInt[index] {
-                case sizeTorah[0]:
-                    newStep.point[0] = 0
-                case -1:
-                    newStep.point[0] = sizeTorah[0] - 1
-                case 0..<sizeTorah[0]:
-                    newStep.point[0] += wayInt[index]
-                default:
-                    print("error")
-                }
-            } else {
-                switch newStep.point[1] + wayInt[index] {
-                case sizeTorah[1]:
-                    newStep.point[1] = 0
-                case -1:
-                    newStep.point[1] = sizeTorah[1] - 1
-                case 0..<sizeTorah[1]:
-                    newStep.point[1] += wayInt[index]
-                default:
-                    print("error")
-                }
-            }
-            
-            stepVerification(step: newStep)
-            if checkStep.finish {
-                finishStep = true
-                break
-            }
-            if checkStep.stepWay && !noDoublePoint.contains(newStep.point) {
-                stepWay.append(newStep)
-                noDoublePoint.insert(newStep.point)
-                total += 1
+        } else {
+            switch newStep.point[1] + wayInt[index] {
+            case sizeTorah[1]:
+                newStep.point[1] = 0
+            case -1:
+                newStep.point[1] = sizeTorah[1] - 1
+            case 0..<sizeTorah[1]:
+                newStep.point[1] += wayInt[index]
+            default:
+                print("error")
             }
         }
+        stepVerification(step: newStep)
         
         if checkStep.finish {
             finishStep = true
+            shortPath = newStep.pathTraveled
             break
         }
         
-        if total == 0  {
-            noWay = true
-            shortPath = " -1"
-        } else {
-            total += 1
+        if checkStep.stepWay {
+            let costPath = costSoFar(currentPoint: newStep.point, sizeTorah: sizeTorah, finish: finishPoint) + costSoFar(currentPoint: newStep.point, sizeTorah: sizeTorah, finish: startPoint)
+            newStep.stepCost = costPath
+            stepWay.append(newStep)
         }
     }
-    
     completedStep = stepWay
 }
 
+var countPath = wayPoints.last!.pathTraveled.count
 
+func findingPath() {
+    var minCostPath = wayPoints.last!.stepCost
+    var minCostPathPoint = wayPoints.last!
+    var passedPointIndex = wayPoints.count - 1
+    for index in wayPoints.indices {
+        if wayPoints[index].pathTraveled.count < countPath  {
+            continue
+        }
+        if wayPoints[index].stepCost < minCostPath {
+            minCostPath = wayPoints[index].stepCost
+            minCostPathPoint = wayPoints[index]
+            passedPointIndex = index
+        }
+    }
+    countPath = minCostPathPoint.pathTraveled.count - 2
+    findingPath(way: minCostPathPoint)
+    //    print("minCostPath = \(minCostPath), minCostPathPoint = \(minCostPathPoint)")
+    passedPoints.insert(wayPoints[passedPointIndex].point)
+    wayPoints.remove(at: passedPointIndex)
+    for item in completedStep {
+        if passedPoints.contains(item.point) {
+            continue
+        }
+        wayPoints.append(item)
+    }
+}
 
 while !finishStep && !noWay {
-    findingPath(way: completedStep)
-    noDoublePoint.removeAll()
-    if sizeTorah[0] > 0 && sizeTorah[1] > 0 {
-        for point in completedStep {
-            noDoublePoint.insert(point.point)
-        }
+    findingPath()
+    if wayPoints.count == 0  {
+        noWay = true
+        shortPath = " -1"
     }
 }
 shortPath.removeFirst()
 print(shortPath)
-
